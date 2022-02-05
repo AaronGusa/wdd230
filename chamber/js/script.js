421245fd0e145bcff2cb3c813362ffa12e6600d5
Goosa2025 <gus21002@byui.edu>
2022-02-05 13:06:49 -0700

    Updated Chambers site, javascript

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -12,3 +12,13 @@ const datefield = document.querySelector("#today");
 const now = new Date();
 const today = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
 datefield.innerHTML = today;
+
+// get the day
+dayOfWeek = now.getDay(); //returns day number 0-6
+
+// if the day is Mon or Tues place banner
+if (dayOfWeek === 1) {
+    document.getElementsByClassName("banner")[0].classList.toggle("bannershow");
+} else if (dayOfWeek === 2) {
+    document.getElementsByClassName("banner")[0].classList.toggle("bannershow");
+}
