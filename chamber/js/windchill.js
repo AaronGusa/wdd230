53820f9804cad93c03ae5704d43ddbf2ca063ce2
Goosa2025 <gus21002@byui.edu>
2022-02-09 17:49:49 -0700

    Updated Portal

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -6,7 +6,6 @@ console.log(windy);
 
 let windyChill = Math.round ((35.74 + (0.6215 * temp) - (35.75 * (windy^0.16) + (0.4275 * (temp * (windy^0.16))))));
 
-
 // check for <= 50F and >3.0mph_
 if (temp <= 50 && temp > 3.0) {
     // display the values 
@@ -14,7 +13,5 @@ if (temp <= 50 && temp > 3.0) {
 } else {
     // display the N/A
     document.getElementById('windChill').innerHTML = `Wind Chill: N/A`;
-
 };
 
-// 
