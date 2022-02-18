8d11d19604dd8874b7339460ca8d5269ed62dc59
Goosa2025 <gus21002@byui.edu>
2022-02-18 00:39:29 -0700

    image optimizations, transitions

--- chamber/js/daysBetween.js
+++ chamber/js/daysBetween.js
@@ -3,7 +3,7 @@ let currentTime = Date.now();
 
 let storageTime = localStorage.getItem("visited-last");
 
-let timeBetween = currentTime - storageTime
+let timeBetween = currentTime - storageTime;
 
 const dayFactor = 1000 * 60 * 60 * 24;
 const hourFactor = 1000 * 60 * 60;
@@ -12,11 +12,16 @@ const secondFactor = 1000;
 
 let daysBetween = Number(timeBetween / dayFactor);
 let hoursBetween = Number(timeBetween / hourFactor);
-let minsBetween = Number(timeBetween / minFactor)
-let secondsBetween = Number(timeBetween / secondFactor)
+let minsBetween = Number(timeBetween / minFactor);
+let secondsBetween = Number(timeBetween / secondFactor);
+
+daysBetween = daysBetween.toFixed(2);
+hoursBetween = hoursBetween.toFixed(2);
+minsBetween = minsBetween.toFixed(2);
+secondsBetween = secondsBetween.toFixed(2);
 
 if (daysBetween >= 1) {
-    document.getElementById('days').textContent = "Welcome!! If you come back later I'll tell you how long it's been!";
+    document.getElementById('days').textContent = "If you come back later I'll tell you how long it's been!";
 }   
 else if (hoursBetween >= 1) {
     document.getElementById('days').textContent = `It's been ${hoursBetween} hours since you last visited.`;
