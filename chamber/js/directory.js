d9803097ab754d02f3a5867a5472018eb0ab3b6d
Goosa2025 <gus21002@byui.edu>
2022-03-04 02:22:25 -0700

    data updating

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,1 +1,13 @@
 const requestURL = "https://goosa2025.github.io/wdd230/chamber/data/data.json";
+const cards = document.querySelector(".cards");
+
+fetch(requestURL)
+    .then(function (response){
+        return response.json();
+
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); //temporary checking for valid response and data parsing
+        const businesses = jsonObject['businesses'];
+        businesses.forEach(displayProphets);
+});
